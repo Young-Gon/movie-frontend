448b93b3e825171f27845827c76e9dd8be97b29d
이영곤 <yg1027@gmail.com>
2020-06-10 02:56:41 +0900

    axios server URL 변경

--- utils/api.ts
+++ utils/api.ts
@@ -3,8 +3,7 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 let $axios: NuxtAxiosInstance
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
-  axiosInstance.defaults.baseURL =
-    "http://ec2-13-209-30-24.ap-northeast-2.compute.amazonaws.com:8080"
+  axiosInstance.defaults.baseURL = "https://gondev.xyz"
   $axios = axiosInstance
 }
 
