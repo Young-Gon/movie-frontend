7d8fc8afb43b3f7d4eb72edec9a1bc0d7b9923fb
이영곤 <yg1027@gmail.com>
2020-07-02 02:08:09 +0900

    process.env.baseUrl 테스트

--- utils/api.ts
+++ utils/api.ts
@@ -3,7 +3,9 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 let $axios: NuxtAxiosInstance
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
-  axiosInstance.defaults.baseURL = "https://gondev.xyz"
+  console.log(process.env.baseUrl)
+  axiosInstance.defaults.baseURL = process.env.baseUrl
+  // "http://ec2-13-209-30-24.ap-northeast-2.compute.amazonaws.com:8080"
   $axios = axiosInstance
 }
 
