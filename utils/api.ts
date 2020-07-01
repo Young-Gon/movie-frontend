34cc93512d9aaf8898b977ee3ba791c7c4f0aa61
이영곤 <yg1027@gmail.com>
2020-07-02 02:17:15 +0900

    BASE_URL 작똥!

--- utils/api.ts
+++ utils/api.ts
@@ -3,9 +3,7 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 let $axios: NuxtAxiosInstance
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
-  console.log(process.env.baseUrl)
   axiosInstance.defaults.baseURL = process.env.baseUrl
-  // "http://ec2-13-209-30-24.ap-northeast-2.compute.amazonaws.com:8080"
   $axios = axiosInstance
 }
 
